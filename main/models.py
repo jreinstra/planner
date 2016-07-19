6ec4753ffcabdbdad24f06ea454c2eb7a34dd286
yohanstra <jreinstra@gmail.com>
2016-07-18 19:21:40 -0700

    Initial commit with some initial models

---
+++ main/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Class(models.Model):
+    pass
+
+class Instructor(models.Model):
+    pass
+
+class Student(models.Model):
+    pass
+
+class Review(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
