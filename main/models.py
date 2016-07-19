6e43712f6356dd4b1f5660e268871933c18c4542
yohanstra <jreinstra@gmail.com>
2016-07-18 22:01:19 -0700

    Continued design of models

--- main/models.py
+++ main/models.py
@@ -3,6 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    pass
+
+class Requirement(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.ForeignKey(Subject, related_name="requirements")
+    eligible_classes = models.ManyToManyField('Class', related_name="requirements")
+    num_units = models.IntegerField()
+    num_classes = models.IntegerField()
+    
 class Class(models.Model):
     pass
 
