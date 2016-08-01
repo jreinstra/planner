f01cdf16fb5ef21c17f7194adecfb442392a74c5
yohanstra <jreinstra@gmail.com>
2016-08-01 10:39:15 -0700

    finalize crawling script

--- create_courses.py
+++ create_courses.py
@@ -11,7 +11,7 @@ application = get_wsgi_application()
 from main.models import *
 
 def populate_departments():
-    schools = get_xml_s("http://explorecourses.stanford.edu/?view=xml-20120105")
+    schools = get_xml("http://explorecourses.stanford.edu/?view=xml-20120105")
     for school in schools.findall("school"):
         school_name = school.get("name")
         if School.objects.filter(name=school_name).exists() is False:
@@ -31,70 +31,73 @@ def populate_departments():
                 
 def populate_courses():
     depts = Department.objects.all()
-    #print dept_codes
-    courses = get_xml_f("http://explorecourses.stanford.edu/search?view=xml-20140630&filter-coursestatus-Active=on&page=0&catalog=&q=AA")
-    # this doesn't get anything ... 
-    #print "length:", len(courses.get("xml").get("courses").findall("course"))
-    for course in courses[2].findall("course"):
-        admin = course.findall("administrativeInformation")[0]
-        course_id = int(admin[0].text)
-        # TODO: check if courses of different years have different IDs
-        if Course.objects.filter(course_id=course_id).exists() is False:
-            c = Course()
-            c.course_id = course_id
-            c.year = course[0].text
-            c.long_name = course[3].text.split("(")[0].strip()
-            c.description = course[4].text or "No description provided."
-            c.repeatable = False if course[6].text == "false" else True
-            c.grading = course[7].text
-            c.min_units = int(course[8].text)
-            c.max_units = int(course[9].text)
-            c.department = depts[0]
-            c.save()
-            for section in course[11].findall("section"):
-                se = CourseSection()
-                se.term = section[1].text
-                se.section_number = int(section[6].text)
-                se.num_enrolled = int(section[8].text)
-                se.max_enrolled = int(section[9].text)
-                se.num_waitlist = int(section[10].text)
-                se.max_waitlist = int(section[11].text)
-                se.enroll_status = section[12].text
-                se.course = c
-                
-                schedule = section[16][0]
-                if len(schedule[6]) > 0:
-                    instructor = schedule[6][0]
-                    instructor_obj = Instructor.objects.filter(sunet=instructor[4].text)
-                    if instructor_obj.exists() is False:
-                        instructor_obj = Instructor()
-                        instructor_obj.sunet = instructor[4].text
-                        instructor_obj.name = instructor[1].text + " " + instructor[3].text
-                        instructor_obj.save()
-                    else:
-                        instructor_obj = instructor_obj[0]
-                    se.instructor = instructor_obj
-                se.save()
-                
-                sc = CourseSchedule()
-                sc.start_date = schedule[0].text or ""
-                sc.end_date = schedule[1].text or ""
-                sc.start_time = schedule[2].text or ""
-                sc.end_time = schedule[3].text or ""
-                sc.days = schedule[5].text
-                sc.section = se
-                sc.save()
-        else:
-            c = Course.objects.get(course_id=course_id)
-        
-        code_str = course[1].text + course[2].text
-        code = CourseCode.objects.filter(code=code_str)
-        if code.exists() is False:
-            code = CourseCode()
-            code.code = code_str
-            code.title = course[3].text
-            code.course = c
-            code.save()
+    
+    # Only load five depts for now (to save time)
+    for dept in depts[:5]:
+        print "Loading courses for:", dept.code
+        courses = get_xml("http://explorecourses.stanford.edu/search?view=xml-20140630&filter-coursestatus-Active=on&page=0&catalog=&q=" + dept.code)
+        # this doesn't get anything ... 
+        #print "length:", len(courses.get("xml").get("courses").findall("course"))
+        for course in courses[2].findall("course"):
+            admin = course.findall("administrativeInformation")[0]
+            course_id = int(admin[0].text)
+            # TODO: check if courses of different years have different IDs
+            if Course.objects.filter(course_id=course_id).exists() is False:
+                c = Course()
+                c.course_id = course_id
+                c.year = course[0].text
+                c.long_name = course[3].text.split("(")[0].strip()
+                c.description = course[4].text or "No description provided."
+                c.repeatable = False if course[6].text == "false" else True
+                c.grading = course[7].text
+                c.min_units = int(course[8].text)
+                c.max_units = int(course[9].text)
+                c.department = dept
+                c.save()
+                for section in course[11].findall("section"):
+                    se = CourseSection()
+                    se.term = section[1].text
+                    se.section_number = int(section[6].text)
+                    se.num_enrolled = int(section[8].text)
+                    se.max_enrolled = int(section[9].text)
+                    se.num_waitlist = int(section[10].text)
+                    se.max_waitlist = int(section[11].text)
+                    se.enroll_status = section[12].text
+                    se.course = c
+
+                    schedule = section[16][0]
+                    if len(schedule[6]) > 0:
+                        instructor = schedule[6][0]
+                        instructor_obj = Instructor.objects.filter(sunet=instructor[4].text)
+                        if instructor_obj.exists() is False:
+                            instructor_obj = Instructor()
+                            instructor_obj.sunet = instructor[4].text
+                            instructor_obj.name = instructor[1].text + " " + instructor[3].text
+                            instructor_obj.save()
+                        else:
+                            instructor_obj = instructor_obj[0]
+                        se.instructor = instructor_obj
+                    se.save()
+
+                    sc = CourseSchedule()
+                    sc.start_date = schedule[0].text or ""
+                    sc.end_date = schedule[1].text or ""
+                    sc.start_time = schedule[2].text or ""
+                    sc.end_time = schedule[3].text or ""
+                    sc.days = schedule[5].text
+                    sc.section = se
+                    sc.save()
+            else:
+                c = Course.objects.get(course_id=course_id)
+
+            code_str = course[1].text + course[2].text
+            code = CourseCode.objects.filter(code=code_str)
+            if code.exists() is False:
+                code = CourseCode()
+                code.code = code_str
+                code.title = course[3].text
+                code.course = c
+                code.save()
                 
 # next: http://explorecourses.stanford.edu/search?view=xml-20140630&filter-coursestatus-Active=on&page=0&catalog=&q=AA
 
@@ -105,11 +108,11 @@ def get_xml(url):
     # x = str(r.text.encode("utf-8"))
     return ElementTree.fromstring(str(r.text.encode("utf-8")))
 
-def get_xml_f(url):
+"""def get_xml_f(url):
     return ElementTree.fromstring(open("courses.xml", "r").read())
 
 def get_xml_s(url):
-    return ElementTree.fromstring(open("schools.xml", "r").read())
+    return ElementTree.fromstring(open("schools.xml", "r").read())"""
     
 class BadAPIError(Exception):
     pass
