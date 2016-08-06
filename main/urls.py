51fb58629c6d0adf67fcd24344a7281a149332b0
yohanstra <jreinstra@gmail.com>
2016-08-05 23:31:42 -0700

    Changed URL scheme

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,10 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    # Main pages
     url(r'^$', TemplateView.as_view(template_name="main/index.html"), name="index"),
-    url(r'^search/$', views.Search.as_view(), name="search"),
     url(r'^courses/(?P<pk>[0-9]+)/$', views.CourseDetailView.as_view(), name='course_detail'),
+    
+    # API routes
+    url(r'^api/search/$', views.Search.as_view(), name="search"),
 ]
