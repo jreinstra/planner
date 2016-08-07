67d63295dfec97ca44501cbe13ed77640278a04b
yohanstra <jreinstra@gmail.com>
2016-08-07 12:20:56 -0700

    Changed serializer fields for Course

--- main/serializers.py
+++ main/serializers.py
@@ -7,7 +7,7 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = (
-            'year', 'title', 'description', 'general_requirements',
-            'repeatable', 'grading', 'min_units', 'max_units', 'department',
+            'title', 'description', 'general_requirements', 'repeatable',
+            'grading', 'min_units', 'max_units', 'department', 'sections'
         )
         depth = 1
