b22f6e557b1a93016a1727583ffac4325c7a8572
yohanstra <jreinstra@gmail.com>
2016-08-14 20:15:04 -0700

    Make trailing slashes optional for API

--- main/urls.py
+++ main/urls.py
@@ -8,9 +8,9 @@ router.register(r'comments', views.CommentViewSet)
 
 urlpatterns = [
     # API routes
-    url(r'^search/$', views.Search.as_view(), name="api_search"),
-    url(r'^courses/(?P<pk>[0-9]+)/$', views.CourseDetail.as_view(), name="api_course_detail"),
-    url(r'^instructors/(?P<sunet>[\w-]+)/$', views.InstructorDetail.as_view(), name="api_instructor_detail"),
+    url(r'^search/?$', views.Search.as_view(), name="api_search"),
+    url(r'^courses/(?P<pk>[0-9]+)/?$', views.CourseDetail.as_view(), name="api_course_detail"),
+    url(r'^instructors/(?P<sunet>[\w-]+)/?$', views.InstructorDetail.as_view(), name="api_instructor_detail"),
     
     # API router
     url(r'^', include(router.urls))
