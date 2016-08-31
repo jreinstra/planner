dc87eec6df48276a262e67da39cef0a169c78a07
akshay2598 <akshayproductions@gmail.com>
2016-08-31 04:34:49 +0000

    Initialize Frontend

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
-    url(r'^', TemplateView.as_view(template_name="frontend/index.html"), name="index")
+    url(r'^$', TemplateView.as_view(template_name="frontend/index.html"), name="index"),
+    url(r'^(?!api).*$', RedirectView.as_view(url='/'))
 ]
